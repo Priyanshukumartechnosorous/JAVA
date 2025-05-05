35decc39a7a6d6b646505bc5922d9245d10e1cb2
Priyanshu Kumarteotia <priyanshukumarteotia11@gmail.com>
2025-05-05 11:20:06 +0530

    polymorphism overloading

---
+++ Overloading.java
@@ -0,0 +1,21 @@
+public class Overloading {
+    public static void main(String[] args) {
+        Test t= new Test();
+        t.max(10,5);
+        t.max(2,3,4);
+    }
+    
+}
+
+
+class Test {
+    public int max(int a, int b) {
+        return a>b ? a:b;
+    }
+    public int max (int a, int b, int c) {
+        if (a>b && a>c) return a;
+        else if (b>c) return b;
+        return c;
+    }
+}
+
