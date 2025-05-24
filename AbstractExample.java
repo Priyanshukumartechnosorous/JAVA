6b4e3d2076bbcbdaa50d8dc4b5b070bfea2d61c6
Priyanshu Kumarteotia <priyanshukumarteotia11@gmail.com>
2025-05-24 10:09:31 +0530

    area os shapes abstract method

---
+++ AbstractExample.java
@@ -0,0 +1,32 @@
+public class AbstractExample {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.lenght=10;
+        r.breadth=5;
+        System.out.println(r.area()); 
+    }
+    
+}
+abstract class Shape {
+    abstract double perimeter();
+    abstract double area();
+}
+class Circle extends Shape {
+    double radius;
+    double perimeter(){
+        return 2*Math.PI*radius;
+    }
+    double area() {
+        return Math.PI*radius*radius;
+    }
+}
+class Rectangle extends Shape {
+    double lenght;
+    double breadth;
+    double perimeter() {
+        return 2*(lenght+breadth);
+    }
+    double area() {
+        return lenght* breadth;
+    }
+}
