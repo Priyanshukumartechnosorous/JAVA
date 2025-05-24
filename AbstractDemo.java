c414931d3162577e04c1c8851b377ca830d4a770
Priyanshu Kumarteotia <priyanshukumarteotia11@gmail.com>
2025-05-24 09:34:36 +0530

    AbstractDemo

---
+++ AbstractDemo.java
@@ -0,0 +1,27 @@
+public class AbstractDemo {
+    public static void main(String[] args) {
+        Super S1;
+        Sub S2= new Sub();
+
+    }    
+}
+
+abstract class Super {
+    Super() {
+        System.out.println("super");
+    }
+    void meth1() {
+        System.out.println("super method1");
+    }
+    abstract void meth2();
+
+    
+}
+class Sub extends Super {
+    void meth2() {
+        System.out.println("sub method 2");
+    }
+
+
+    
+}
